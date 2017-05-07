0616f9f701f9d2518384d7da07104e0ca774510c
nicolelee63 <nicole.lee633@gmail.com>
2017-05-08 01:33:15 +0800

    checkpoint 6 completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl(Room, Message, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal, $cookies) {
       var home = this;
       this.rooms = Room.all;
       home.currentRoom = null;
+      home.currentUser = $cookies.get('blocChatCurrentUser');
 
       home.addRoom = function() {
         $uibModal.open({
@@ -15,11 +16,17 @@
       home.setCurrentRoom = function (room) {
              home.currentRoom = room;
              home.messages = Message.getByRoomId(home.currentRoom.$id);
-             console.log(home.messages)
          }
+
+      home.sendMessage = function (){
+        home.newMessage.roomId = home.currentRoom.$id;
+        home.newMessage.username = home.currentUser;
+        Message.send(home.newMessage);
+        home.newMessage.content = null;
+        }
     }
 
     angular
         .module('bloc-chat')
-        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal','$cookies', HomeCtrl]);
 })();
